7ca0c01587047ea12d12a905afd67e4bf52b07f9
Jhon Garcia <metalprime57@gmail.com>
2020-12-11 09:49:54 -0500

    update index js

---
+++ script/index.js
@@ -0,0 +1,20 @@
+const welcome__user = document.querySelector('.welcome__user');
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid;
+      // ...
+      db.ref('administradores&/'+uid).on('value', element =>{
+          const value = element.val();
+
+          welcome__user.innerHTML = value.nombre;
+      });
+
+    } else {
+      // User is signed out
+      // ...
+      window.location.href = './login.html';
+    }
+  });
